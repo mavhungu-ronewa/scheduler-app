b1b6000ae050f24b26daba3652b620b4973afacf
ronewa mavhungu <ronewamavhungu.rm@gmail.com>
2023-10-13 18:01:58 +0200

    commenting out Product && Category context

--- src/hooks/useFetchSurveyData.jsx
+++ src/hooks/useFetchSurveyData.jsx
@@ -2,22 +2,24 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function useFetchSurveyData(dataSource){
-  const [surveyDatas, setSurveyData] = useState([])
+  const [surveyDatas, setSurveyData] = useState([]);
   const [isLoading,setIsLoading] = useState(true);
   const [error, setError ] = useState();
 
   useEffect(() => {
-    const fetchSurveyData = async () => {
+    async function fetchSurveyData(){
       try {
         const response = await axios.get(dataSource);
-        setSurveyData(response.data);
-        setIsLoading(false);
+        if(response.data){
+          setSurveyData(response.data);
+          setIsLoading(false);
+        }
       } catch (error) {
         console.error('Error:', error);
         setError(error);
         setIsLoading(false);
       }
-    };
+    }
     fetchSurveyData();
   }, [dataSource]);
   return [isLoading, error, surveyDatas ];
--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,22 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx';
 import './index.css';
 import { SnackbarProvider } from "notistack";
-import { ProductsContextProvider } from "./context/productsContext";
-import { CategoryContextProvider } from "./context/CategoryContext.jsx";
+/*import { ProductsContextProvider } from "./context/productsContext";
+import { CategoryContextProvider } from "./context/CategoryContext.jsx";*/
 import { SurveyContextProvider } from "./context/SurveyContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProductsContextProvider>
-        <CategoryContextProvider>
+      {/*<ProductsContextProvider>
+        <CategoryContextProvider>*/}
           <SurveyContextProvider>
             <SnackbarProvider>
               <App />
             </SnackbarProvider>
           </SurveyContextProvider>
-        </CategoryContextProvider>
-      </ProductsContextProvider>
+       {/* </CategoryContextProvider>
+      </ProductsContextProvider>*/}
     </BrowserRouter>
   </React.StrictMode>
 );
